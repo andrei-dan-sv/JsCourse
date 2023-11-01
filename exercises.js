57bce516f299148b4a0501ae6f21f4d2169b0b23
Dan <andrei.dan@cognizant.com>
2023-11-01 17:35:17 +0200

    Updates

---
+++ exercises.js
@@ -0,0 +1,30 @@
+// Afisati in consola suma salariilor din obiectul de mai jos
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+
+
+// Creati un calculator folosind Object methods si Object constructors
+function Calculator(firstOperand, secondOperand) {
+
+}
+
+let firstOperand = 2;
+let secondOperand = 5;
+const calculator = new Calculator(1,1);
+// console.log(calculator.sum());
+// console.log(calculator.subtract());
+// console.log(calculator.multiply());
+
+
+// Afisati varsta medie din array-ul de obiecte de mai jos
+const peoples = [{ name: "John", age: 25 },
+    { name: "Pete", age: 16 },
+    { name: "Mary", age: 29 }];
+
+
+// Filtrati array-ul de mai sus afisand doar persoanele majore
+
